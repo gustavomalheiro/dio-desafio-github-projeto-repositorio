fb1ee86820698a49a093871847f6430f98fad7b6
Gustavo Malheiro <gus.malheiro@gmail.com>
2021-10-11 11:42:20 -0300

    iniciando as aulas de estruturas de dados em Java

---
+++ Java/estruturas-de-dados-java/src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -0,0 +1,23 @@
+package com.projeto.atribuicaoreferencia;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // com tipos primitivos
+        int intA = 1;
+        int intB = intA; // copia o valor de intA
+
+        System.out.println("intA= " + intA + " intB= " + intB);
+        intA = 2; // não modifica o valor em intB
+        System.out.println("intA= " + intA + " intB= " + intB);
+
+        // com objetos
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA; // copia a referência em memória de a para b
+
+        System.out.println("objA= " + objA + " objB= " + objB);
+        objA.setNum(2); // modifica o valor em intB
+        System.out.println("objA= " + objA + " objB= " + objB); 
+    }
+}
---
+++ intelij-projects/caracteristicas-da-linguagem/src/main/java/one/digital/innovation/Program.java
@@ -0,0 +1,11 @@
+package one.digital.innovation;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
